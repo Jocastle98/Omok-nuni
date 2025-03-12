7e6776159eb2225b6b1fb8a65ea32f1f848d9e37
ysw4697 <ysw4697@naver.com>
2025-03-12 13:28:03 +0900

    feat: 싱글톤 스크립트 생성
    게임 매니저 구현 중

---
+++ Assets/@02.Scripts/Common/Singleton.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T m_instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = FindObjectOfType<T>();
+                if (m_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    m_instance = obj.AddComponent<T>();
+                }
+            }
+            
+            return m_instance;
+        }
+    }
+    
+    private void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this as T;
+            DontDestroyOnLoad(gameObject);
+            
+            // 씬 전환시 호출되는 액션 메서드 할당
+            
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
