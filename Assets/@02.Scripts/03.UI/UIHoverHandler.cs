47a3c5d4a54238524e75cdc7f2639f44f914533c
pianoop <kphjjh723@gmail.com>
2025-03-29 02:38:35 +0900

    feat: Branch animation: 기본 움직임, 커서 접근시 크게 움직임 구현 및 해당하는 Branch의 SpriteSheet 추가

---
+++ Assets/@02.Scripts/03.UI/UIHoverHandler.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+public class UIHoverHandler : MonoBehaviour, IPointerEnterHandler
+{
+    public Animator Animator;
+    
+    private static readonly int IsMouseOverHash = Animator.StringToHash("isMouseOver");
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (Animator != null)
+        {
+            Animator.SetTrigger(IsMouseOverHash);
+        }
+    }
+}
